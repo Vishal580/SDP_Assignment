2c04489d93dc6fa903e796e2f6ebc264beeca170
vishal <vishal@VISHAL>
2022-05-02 16:33:03 +0530

    Project

---
+++ R20EF440/src/test/java/TestHello.java
@@ -0,0 +1,11 @@
+import org.junit.*;
+public class TestHello {
+	
+	@Test
+	
+	public void testhello() {
+		System.out.println("Hello world");
+		System.out.println("Vishal");
+	}
+
+}
---
+++ R20EF440/src/test/java/TestVishal.java
@@ -0,0 +1,9 @@
+import org.junit.*;
+public class TestVishal {
+	@Test
+	public void testvishal() {
+		
+		System.out.println("Vishal");
+	}
+
+}
